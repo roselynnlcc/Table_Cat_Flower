bde94db0abbbcf94183f015eb6c751e6f327e367
Roselyn Chan <123598560+roselynnlcc@users.noreply.github.com>
2026-05-11 18:51:28 -0600

    Add files via upload

---
+++ tips.js
@@ -0,0 +1,9 @@
+const TCF_TIPS = [
+  {
+    id: 1,
+    title: "Formes nominales — Conclusion",
+    subtitle: "Avantages & Inconvénients",
+    url: "tips/conclusion_nouns.html",
+    date: "2026-05-11"
+  }
+];
